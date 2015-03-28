e208c3dc4efa2abf2d67d3ee5087bd55b9dbb49f
pm489 <pj.meredith@gmail.com>
2015-03-28 17:29:47 +0000

    added a bit of sanity  to the api

--- server/lib/bikes/index.js
+++ server/lib/bikes/index.js
@@ -3,12 +3,18 @@ var bikes = require('./bikes.js');
 var app = module.exports = express();
 
 app.get('/bikes', function (req, res) {
-  var currentLocation = req.param('location')|| "unset";
-  if(currentLocation==="unset"){
-    res.status(501).send("No location defined. api: /bikes?location=");
-  }
+  var currentLocation = req.param('location') || "empty";
 
-  bikes.geocodeLocation(currentLocation).then(function(response){
-    res.send("done" + JSON.stringify(response));
-  }).done();
-});
+  console.log("curr = " + currentLocation);
+  if (currentLocation === "empty") {
+    res.status(501).send("No location defined. api: /bikes?location=")
+  } else {
+    var radius = req.param('radius') || 200;
+    bikes.geocodeLocation(currentLocation).then(function (response) {
+      res.send(JSON.stringify(response));
+    }).catch(function (error) {
+      res.send("error: " + error.message);
+    }).done();
+  }
+})
+;
